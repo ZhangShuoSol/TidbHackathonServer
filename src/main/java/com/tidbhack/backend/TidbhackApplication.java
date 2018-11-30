53fa15918b09a348224abf3cb7b47a9294acd2ad
songwenbin <songwenbin@outlook.com>
2018-11-30 14:22:54 +0800

    init version

---
+++ src/main/java/com/tidbhack/backend/TidbhackApplication.java
@@ -0,0 +1,12 @@
+package com.tidbhack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidbhackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TidbhackApplication.class, args);
+	}
+}
